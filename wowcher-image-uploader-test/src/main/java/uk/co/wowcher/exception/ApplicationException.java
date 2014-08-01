7387d809c8ada4d37b273caac47f2d1ac9ff223b
FrancescoBianchi <francesco.bianchi.os@gmail.com>
2014-08-01 15:50:26 +0200

    Added application ancestor class

---
+++ wowcher-image-uploader-test/src/main/java/uk/co/wowcher/exception/ApplicationException.java
@@ -0,0 +1,10 @@
+package uk.co.wowcher.exception;
+
+public class ApplicationException extends Exception {
+
+	/**
+	 * Servial version UID
+	 */
+	private static final long serialVersionUID = 9207128128566934630L;
+
+}

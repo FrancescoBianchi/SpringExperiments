01fb8b374fdc5e672e487eceb39cd4ed359e12c5
FrancescoBianchi <francesco.bianchi.os@gmail.com>
2014-08-02 00:34:12 +0200

    Merge of Config and WebMvcConfiguration

--- wowcher-image-uploader-test/src/main/java/uk/co/wowcher/config/WebMvcConfiguration.java
+++ wowcher-image-uploader-test/src/main/java/uk/co/wowcher/config/WebMvcConfiguration.java
@@ -8,12 +8,25 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+/**
+ * Deals with the configuration of the MVC functionalities of the application.
+ * We need it only to register the resolver for multipart requests.
+ * 
+ * @author francesco.bianchi
+ */
 @Configuration
-@ComponentScan
+@ComponentScan({"uk.co.wowcher", "uk.co.wowcher.service.impl"})
 @EnableWebMvc
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
-    @Bean
+    
+	/**
+	 * Registration of the bean in charge of parsing the multipart request
+	 * 
+	 * @return the bean in charge of parsing the multipart request
+	 */
+	@Bean
     public MultipartResolver multipartResolver() {
     	return new CommonsMultipartResolver();
     }
+	
 }
